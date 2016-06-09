c993d74a6182c40fc92e9a3c3472ffaf1b46db89
Kehlin Swain <kehlinswain@gmail.com>
2016-06-09 16:57:33 +0000

    Fixed swinging user input and tested.

--- fusion/yaw_pitch_roll_collector.py
+++ fusion/yaw_pitch_roll_collector.py
@@ -1,13 +1,13 @@
 from SF_9DOF import IMU
 from fusion import Fusion
-from swing_detector import SwingDetector 
+from swing_detector import SwingDetector
 import time
 import sys
 import select
 import tty
 import termios
 # Create IMU object
-imu = IMU() # To select a specific I2C port, use IMU(n). Default is 1. 
+imu = IMU() # To select a specific I2C port, use IMU(n). Default is 1.
 
 # Initialize IMU
 imu.initialize()
@@ -19,20 +19,20 @@ imu.enable_gyro()
 imu.enable_temp()
 fuse = Fusion()
 
-callibrate_count = 0 
+callibrate_count = 0
 
-#swing detection 
+#swing detection
 swing = SwingDetector()
 
 # Set range on accel, mag, and gyro
 
 # Specify Options: "2G", "4G", "6G", "8G", "16G"
-imu.accel_range("2G")       # leave blank for default of "2G" 
+imu.accel_range("2G")       # leave blank for default of "2G"
 
 # Specify Options: "2GAUSS", "4GAUSS", "8GAUSS", "12GAUSS"
 imu.mag_range("2GAUSS")     # leave blank for default of "2GAUSS"
 
-# Specify Options: "245DPS", "500DPS", "2000DPS" 
+# Specify Options: "245DPS", "500DPS", "2000DPS"
 imu.gyro_range("245DPS")    # leave blank for default of "245DPS"
 
 
@@ -47,6 +47,7 @@ swinging = False
 
 def is_swinging():
     global swinging
+    print(swinging)
     if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
         c = sys.stdin.read(1)
         if c == '\x1b':
@@ -54,60 +55,59 @@ def is_swinging():
         swinging = not swinging
     return swinging
 
+# this is for non-blocking input
+old_settings = termios.tcgetattr(sys.stdin)
+try:
+    tty.setcbreak(sys.stdin.fileno())
 
-while(1):
-    
-    imu.read_accel()
-    imu.read_mag()
-    imu.read_gyro()
-    imu.readTemp()
+    while(1):
+
+        imu.read_accel()
+        imu.read_mag()
+        imu.read_gyro()
+        imu.readTemp()
 
 
-     #gather the accel results for the fusion algorithm 
-    accel = (float(imu.ax), float(imu.ay), float(imu.az))
-    gyro = (float(imu.gx), float(imu.gy), float(imu.gz))
-    mag = (float(imu.mx), float(imu.my), float(imu.mz))
-
-   
-
-    # Print the results
-    print "Accel: " + str(imu.ax) + ", " + str(imu.ay) + ", " + str(imu.az) 
-    print "Mag: " + str(imu.mx) + ", " + str(imu.my) + ", " + str(imu.mz) 
-    print "Gyro: " + str(imu.gx) + ", " + str(imu.gy) + ", " + str(imu.gz) 
-    print "Temperature: " + str(imu.temp) 
-    
-    data = {"AX":str(imu.ax),"AY":str(imu.ay),"AZ":str(imu.az)}
-    
-    #passes the data to the fusion data for yaw pitch and roll data
-    fuse.update(accel,gyro, mag)
-   # print("I am printing the fuse data", fuse.roll)
-    print("\n")
-    callibrate_count += 1
-
-    #initial callibration
-    if (not callibrated or callibrate_count < 50) and (abs(_heading - fuse.heading) > 1.0 or abs(_pitch - fuse.pitch) > 1.0 or abs(_roll - fuse.roll) > 1.0):
-       #_heading = (_heading * callibrate_count + fuse.heading) / (callibrate_count + 1)
-       #_pitch = (_pitch * callibrate_count + fuse.pitch) / (callibrate_count + 1)
-       #_roll = (_roll * callibrate_count + fuse.roll) / (callibrate_count + 1)
-       _heading = fuse.heading
-       _pitch = fuse.pitch
-       _roll = fuse.roll
-    else:
-       callibrated = True
-       heading = fuse.heading - _heading 
-       pitch = fuse.pitch - _pitch 
-       roll = fuse.roll - _roll  
-       print("Motion Tracking Values!!: Pitch: {:7.3f} Heading: {:7.3f} Roll: {:7.3f}".format(pitch, heading, roll))
-       outFile.write("{:7.3f},{:7.3f},{:7.3f},{:d}\n".format(heading, pitch, roll, is_swinging()))
-       swing.swing_detector(heading, pitch, roll) 
-    print("count is : ", callibrate_count, "\n")
-    # Sleep for 1/10th of a second
-    time.sleep(0.1)
+         #gather the accel results for the fusion algorithm
+        accel = (float(imu.ax), float(imu.ay), float(imu.az))
+        gyro = (float(imu.gx), float(imu.gy), float(imu.gz))
+        mag = (float(imu.mx), float(imu.my), float(imu.mz))
 
 
 
+        # Print the results
+        print "Accel: " + str(imu.ax) + ", " + str(imu.ay) + ", " + str(imu.az)
+        print "Mag: " + str(imu.mx) + ", " + str(imu.my) + ", " + str(imu.mz)
+        print "Gyro: " + str(imu.gx) + ", " + str(imu.gy) + ", " + str(imu.gz)
+        print "Temperature: " + str(imu.temp)
 
+        data = {"AX":str(imu.ax),"AY":str(imu.ay),"AZ":str(imu.az)}
 
+        #passes the data to the fusion data for yaw pitch and roll data
+        fuse.update(accel,gyro, mag)
+       # print("I am printing the fuse data", fuse.roll)
+        print("\n")
+        callibrate_count += 1
 
+        #initial callibration
+        if (not callibrated or callibrate_count < 50) and (abs(_heading - fuse.heading) > 1.0 or abs(_pitch - fuse.pitch) > 1.0 or abs(_roll - fuse.roll) > 1.0):
+           #_heading = (_heading * callibrate_count + fuse.heading) / (callibrate_count + 1)
+           #_pitch = (_pitch * callibrate_count + fuse.pitch) / (callibrate_count + 1)
+           #_roll = (_roll * callibrate_count + fuse.roll) / (callibrate_count + 1)
+           _heading = fuse.heading
+           _pitch = fuse.pitch
+           _roll = fuse.roll
+        else:
+           callibrated = True
+           heading = fuse.heading - _heading
+           pitch = fuse.pitch - _pitch
+           roll = fuse.roll - _roll
+           print("Motion Tracking Values!!: Pitch: {:7.3f} Heading: {:7.3f} Roll: {:7.3f}".format(pitch, heading, roll))
+           outFile.write("{:7.3f},{:7.3f},{:7.3f},{:d}\n".format(heading, pitch, roll, is_swinging()))
+           swing.swing_detector(heading, pitch, roll)
+        print("count is : ", callibrate_count, "\n")
+        # Sleep for 1/10th of a second
+        time.sleep(0.1)
 
-
+finally:
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
