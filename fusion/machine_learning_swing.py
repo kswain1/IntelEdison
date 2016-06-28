b88ca2b601c3438db6ec43734fa8185cecb5323a
kswain1 <kehlinswain@gmail.com>
2016-06-28 17:30:15 +0000

    Added machine learning python script

---
+++ fusion/machine_learning_swing.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+
+# Training data
+data = pd.read_csv('./fusion/logs/accel_isiswing.csv', index_col=0)
+feature_cols = ['Ax', 'Ay', 'Az']
+X = data[feature_cols]
+Y = data['Swing']
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X, Y)
+
+# Predict whether we are swinging or not
+y_pred = knn.predict(X)
+for swing in y_pred:
+    if swing: print("Swinging")
+    else: print("Not Swinging")
+print(metrics.accuracy_score(Y, y_pred))
+
