9624052dddba3b5b93991f26a8bc301e1eb9d081
Christian <cgeweshi460@gmail.com>
2016-09-22 23:42:51 -0700

    Manually implement gravity compensation

--- SwingGenerator/swing_velocity.py
+++ SwingGenerator/swing_velocity.py
@@ -547,7 +547,7 @@ def streamSwingTrial():
         previousElapsedSampleTime += currentElapsedSampleTime  # move to next step
 
         aimAngle = atan(directionMatrix[0][1] / directionMatrix[0][0])
-        roll = currentEulerParameters[0] * 57.3
+        roll = currentEulerParameters[3]
 
         aimAngleVector.append(aimAngle)
         rollVector.append(roll)
