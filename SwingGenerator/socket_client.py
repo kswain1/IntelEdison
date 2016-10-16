67a9b117ed3e8b8a3841fd1c92d2f5263d79d5c0
Christian <cgeweshi460@gmail.com>
2016-10-15 21:57:05 -0700

    Change IP

--- SwingGenerator/socket_client.py
+++ SwingGenerator/socket_client.py
@@ -3,7 +3,7 @@ import socket               # Import socket module
 s = socket.socket()         # Create a socket object
 port = 80                # Reserve a port for your service.
 
-s.connect(('192.168.1.41', port))
+s.connect(('192.168.0.11', port))
 print s.recv(1024)
 s.send("hello you are connected to an intel edison")
 s.close                     # Close the socket when done
