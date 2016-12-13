4fea19c42e46aefdaf4342c170a14a5292281148
Isis Ashford <isisashford@Isiss-MacBook-Pro.local>
2016-12-13 15:10:09 -0600

    unity project

---
+++ Unity/Assets/Scripts/BatPath1.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+
+
+function Update () {
+
+transform.Rotate(Vector3(0,0,0));
+transform.Rotate(Vector3(0,5,2));
+transform.Rotate(Vector3(0,10,9));
+
+//transform.Rotate(Vector3(0,1,0) * Time.deltaTime);
+//transform.Rotate(Vector3(0,6,0) * Time.deltaTime);
+//transform.Rotate(Vector3(0,20,0) * Time.deltaTime);
+//transform.Rotate(0.0, 0.0,0.0, relativeTo: Space = Space.Self);
+////transform.Rotate(xAngle: 3.0, yAngle: 0.0, zAngle: 0.0, relativeTo: Space = Space.Self);
+////transform.Rotate(xAngle: 10.0, yAngle: 0.0, zAngle: 0.0, relativeTo: Space = Space.Self);
+
+}
+
+
+
+
+
+
---
+++ Unity/Assets/Scripts/Batpath2.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class Batpath2 : MonoBehaviour 
+{
+	public TextAsset csvFile; // Reference of CSV file
+	public InputField rollNoInputField;// Reference of rollno input field
+	public InputField nameInputField; // Reference of name input filed
+	public Text contentArea; // Reference of contentArea where records are displayed
+	private char lineSeperater = '\n'; // It defines line seperate character
+	private char fieldSeperator = ','; // It defines field seperate chracter
+	//public string pathName = "/Users/isisashford/Downloads/IntelEdison-master\ 3/euler_parametrization_animation/accel_roll_pitch_demo.csv";
+	//public FileStream fileStream = new FileStream(@"\\Users\\isisashford\\Downloads\\IntelEdison-master\\ 3/\\euler_parametrization_animation\\accel_roll_pitch_demo.csv",FileMode.Open, FileAccess.Read);
+
+//	public string currentDirectory = Directory.GetCurrentDirectory();
+	void Start ()
+	{
+		readData ();
+	}
+
+	// Read data from CSV file
+	private void readData()
+	{
+
+			
+		string[] records = csvFile.text.Split (lineSeperater);
+		foreach (string record in records)
+		{
+			string[] fields = record.Split(fieldSeperator);
+			foreach(string field in fields)
+			{
+				contentArea.text += field + "\t";
+			}
+			contentArea.text += '\n';
+		}
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
