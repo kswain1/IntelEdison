5e0736858dcf31714de5e3344271eac40d7439ac
kehlin swain <kehlinswain@gmail.com>
2017-02-11 15:31:47 -0600

    updates

--- SwingGenerator/machine_learning_swing_no.py
+++ SwingGenerator/machine_learning_swing_no.py
@@ -12,40 +12,31 @@ class IsSwinging(object):
             data_file = "guzman_logs/accel_sfswing.csv"
         if not accel_vector:
             return "please send accel vector ax, ay, az"
-        self._accel_data = pd.read_csv(data_file, index_col=False)
-        self._feature_cols = ['Ax', 'Ay', 'Az']
+        data_file = open("guzman_logs/accel_sfswing.csv","r")
+        read_lines = data_file.readline()
+        read_data = str(read_lines).split()
+        self._feature_cols = []
+        self._output_data = []
         self._accel_vector = accel_vector
+        for row in read_data[1:]:
+            input = row.split(',')
+            row = [float(i) for i in input[:3]]
+            self._feature_cols.append(row[:3])
+            self._output_data.append(int(input[3]))
 
     def is_swinging(self):
         #sets up the training data for classification(swing or not swinging) using nearest neighbor algorithm
-        input = self._accel_data[self._feature_cols]
-        output = self._accel_data['Swing']
+        input = self._feature_cols
+        output = self._output_data
         knn = KNeighborsClassifier(n_neighbors=1)
         knn.fit(input,output)
         accel_point = self._accel_vector
         return knn.predict(self._accel_vector)
 
 
-# # Training data
-# data = pd.read_csv('guzman_logs/accel_sfswing.csv', index_col=False)
-# feature_cols = ['Ax', 'Ay', 'Az']
-# X = data[feature_cols]
-# X.values[0]
-# Y = data['Swing']
-# knn = KNeighborsClassifier(n_neighbors=1)
-# knn.fit(X, Y)
-#
-# # Predict whether we are swinging or not
-# y_pred = knn.predict(X)
-# print knn.predict([1,1,1])
-# for swing in y_pred:
-#     if swing: print("Swinging")
-#     else: print("Not Swinging")
-# print(metrics.accuracy_score(Y, y_pred))
-
 #Example on how to use the class
-#ami = IsSwinging(accel_vector = [1,1,1])
-#print ami.is_swinging()
+# ami = IsSwinging(accel_vector = [1,1,1])
+# print ami.is_swinging()
 
 
 
